ac1f03e182f1e04284c2d0c1691388ad8ef0722d
Armandocarrillo <ingcompuarmando@gmail.com>
2020-01-09 23:11:22 -0600

    picker

--- currencyConverter WatchKit Extension/InterfaceController.swift
+++ currencyConverter WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,23 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var amountLabel: WKInterfaceLabel!
+    @IBOutlet var amountSlider: WKInterfaceSlider!
+    @IBOutlet var currencyPicker: WKInterfacePicker!
+    static let currencies = ["USD", "AUD", "CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "JPY", "SGD"]//opciones para picker
+    static let defaultCurrencies = ["USD", "EUR"]
+    var currentCurrency = "USD"
+    var currentAmount = 500
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        var items = [WKPickerItem]()//metodo para llamar al picker *//
+        for currency in InterfaceController.currencies { let item = WKPickerItem()
+        item.title = currency
+        items.append(item)
+        }
+        currencyPicker.setItems(items)//*
         
-        // Configure interface objects here.
     }
     
     override func willActivate() {
@@ -28,4 +41,16 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func convertTapped() {
+    }
+    
+    @IBAction func amountChanged(_ value: Float) {
+        currentAmount = Int(value) //convvierte el valor de float a int
+        amountLabel.setText(String(currentAmount))//muestra el valor currentAmount conertido en string en amountLabel
+    }
+    
+    @IBAction func baseCurrencyChanged(_ value: Int) {
+         currentCurrency = InterfaceController.currencies[value]//
+    }
+    
 }
